8903027a9843f4894639eaf152f58234dd7185a5
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2023-11-17 23:29:29 +0600

    Added Employee pages

--- Main Project/hrm/resources/views/backend/layout/partials/left_sidebar.blade.php
+++ Main Project/hrm/resources/views/backend/layout/partials/left_sidebar.blade.php
@@ -42,110 +42,11 @@
                 </li>
 {{-- Employees --}}
                 <li>
-                    <a href="">
+                    <a href="{{route('employee.index')}}">
                         <i class="fa fa-users"></i>
                         <span>Employees</span>
                     </a>
                 </li>
-{{--  Admin --}}
-                <li>
-                    <a href="index.html">
-                        <i class="fa fa-users"></i>
-                        <span>Admin</span>
-                    </a>
-                </li>
-{{-- Department --}}
-                <li>
-                    <a href="index.html">
-                        <i class="fa fa-briefcase"></i>
-                        <span>Department</span>
-                    </a>
-                </li>
-{{-- Holidays --}}
-                <li>
-                    <a href="index.html">
-                        <i class="fa fa-send"></i>
-                        <span>Holidays</span>
-                    </a>
-                </li>
-
-                
-
-{{-- Attendance --}}
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-window-section"></i>
-                        <span>Attendance</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true">
-                        <li>
-                            <a href="javascript: void(0);" >Make Attendance</a>
-                        </li>
-                        <li>
-                            <a href="javascript: void(0);" >View Attendance</a>
-                        </li>
-                        <li>
-                            <a href="javascript: void(0);">Leave Types</a>
-                        </li>
-                    </ul>
-                </li>
-{{-- Leave Application --}}
-                <li>
-                    <a href="index.html">
-                        <i class="uil-home-alt"></i>
-                        <span>Leave Application</span>
-                    </a>
-                </li>
-
-{{-- Notice Board --}}
-                <li>
-                    <a href="index.html">
-                        <i class="uil-home-alt"></i>
-                        <span>Notice Board</span>
-                    </a>
-                </li>
-{{-- Settings --}}
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="uil-window-section"></i>
-                        <span>Settings</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true">
-                        <li>
-                            <a href="javascript: void(0);" class="has-arrow">Vertical</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-dark-sidebar.html">Dark Sidebar</a></li>
-                                <li><a href="layouts-compact-sidebar.html">Compact Sidebar</a></li>
-                                <li><a href="layouts-icon-sidebar.html">Icon Sidebar</a></li>
-                                <li><a href="layouts-boxed.html">Boxed Width</a></li>
-                                <li><a href="layouts-preloader.html">Preloader</a></li>
-                                <li><a href="layouts-colored-sidebar.html">Colored Sidebar</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="javascript: void(0);" class="has-arrow">Horizontal</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-horizontal.html">Horizontal</a></li>
-                                <li><a href="layouts-hori-topbar-dark.html">Topbar Dark</a></li>
-                                <li><a href="layouts-hori-boxed-width.html">Boxed Width</a></li>
-                                <li><a href="layouts-hori-preloader.html">Preloader</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="menu-title">Apps</li>
-
-                <li>
-                    <a href="calendar.html" class="waves-effect">
-                        <i class="uil-calender"></i>
-                        <span>Calendar</span>
-                    </a>
-                </li>
-
-
-
             </ul>
         </div>
         <!-- Sidebar -->
